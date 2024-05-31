09bdd9e1bf435f12f52ea1f957af0b62039b8967
mint73 <133520018+mint73@users.noreply.github.com>
2024-06-01 08:35:15 +0900

    change: item

--- src/data.tsx
+++ src/data.tsx
@@ -1,10 +1,7 @@
 //ここに商品のデータを入力してください
 //スペースを入れないように気を付けてください
 export const productData = [
-  { product: "パン100円", price: "100" },
   { product: "パン110円", price: "110" },
-  { product: "パン120円", price: "120" },
-  { product: "パン130円", price: "130" },
   { product: "パン140円", price: "140" },
   { product: "パン150円", price: "150" },
   { product: "パン160円", price: "160" },
@@ -13,13 +10,12 @@ export const productData = [
   { product: "パン190円", price: "190" },
   { product: "パン200円", price: "200" },
   { product: "パン210円", price: "210" },
-  { product: "パン220円", price: "220" },
   { product: "アイス50円", price: "50" },
   { product: "アイス80円", price: "80" },
   { product: "アイス100円", price: "100" },
   { product: "アイス120円", price: "120" },
   { product: "アイス150円", price: "150" },
-  { product: "ドリンク130円", price: "130" },
   { product: "ドーナツ130円", price: "130" },
   { product: "ドーナツ140円", price: "140" },
+  { product: "ドリンク130円", price: "130" },
 ];
